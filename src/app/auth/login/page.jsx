b338cf753e6896104d1f26f7507bd4b1ac8852f8
ElyzabelleRollin <1720870@cstj.qc.ca>
2024-11-11 10:51:09 -0500

    Oauth working

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,15 +1,14 @@
 import { oauthSigninAction } from "@/app/_actions/auth";
 import React from "react";
-import { createClient } from "@/app/_lib/supabase/client";
-import AllButton from "@/_components/AllButton"
+
 
 const LoginPage = async () => {
-  const supabase =  await createClient();
-  console.log(supabase);
   return (
     <div className="p-4">
       <h1 className="text-4xl mb-4 mt-4">Page d'authentification</h1>
-      <AllButton name="Connexion avec Github"  action={oauthSigninAction}/>
+      <form action={oauthSigninAction}>
+        <button>Connexion avec Github</button>
+      </form>
     </div>
   );
 };
