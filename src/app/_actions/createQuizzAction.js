253060af1ad48f97f4d52d57d4f1dcc5f72a98dc
amelie <1951472@cstj.qc.ca>
2024-11-18 09:42:10 -0500

    fix-redirection-when-creating-new-quizz

--- src/app/_actions/createQuizzAction.js
+++ src/app/_actions/createQuizzAction.js
@@ -11,21 +11,23 @@ export const createQuizzAction = async (formData) => {
     //pour communiquer avec la db
     const supabase = createClient();
 
-    //INSERT new quizz
-    const { error } = await supabase.from("quizzes").insert({
+    //INSERT new quizz remplacer par upsert, .select(slug)
+    const { data: newQuizz, error } = await supabase.from("quizzes").upsert({
         name: title,
         description: description,
-    });
+    }).select().single();
+
+    console.log(newQuizz.slug);
 
     //get the last quizz created
-    const { data: quizz, error: quizzError } = await supabase.from("quizzes")
-        .select()
-        .order('created_at', { ascending: false })
-        .limit(1)
-        .single();
-    console.log(quizz);
-    console.log(quizz.slug);
+    // const { data: quizz, error: quizzError } = await supabase.from("quizzes")
+    //     .select()
+    //     .order('created_at', { ascending: false })
+    //     .limit(1)
+    //     .single();
+    // console.log(quizz);
+    // console.log(quizz.slug);
 
     //redirect vers la page de modification du quizz
-    redirect(`/quizz/${quizz.slug}/edit`);
+    redirect(`/quizz/${newQuizz.slug}/edit`);
 };
