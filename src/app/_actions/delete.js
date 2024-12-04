8f4d170bfc739bcfdd9a7230fda7c27d05677122
ElyzabelleRollin <1720870@cstj.qc.ca>
2024-12-03 20:54:15 -0500

    comment delete function

--- src/app/_actions/delete.js
+++ src/app/_actions/delete.js
@@ -3,11 +3,11 @@ import { revalidatePath } from "next/cache";
 import { createClient } from "../_lib/supabase/server";
 
 export const deleteQuiz = async (id) => {
-  const supabase = createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const { error } = await supabase.from("quizzes").delete().eq("id", id);
-  console.log("[QUIZ DELETED]:", error);
-  revalidatePath("/application/profiles/" + user.id);
+  //   const supabase = createClient();
+  //   const {
+  //     data: { user },
+  //   } = await supabase.auth.getUser();
+  //   const { error } = await supabase.from("quizzes").delete().eq("id", id);
+  //   console.log("[QUIZ DELETED]:", error);
+  //   revalidatePath("/application/profiles/" + user.id);
 };
