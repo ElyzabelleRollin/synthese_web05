5efb5272b938df9d609a2446dc2612f8ebd5663b
ElyzabelleRollin <1720870@cstj.qc.ca>
2024-12-09 12:53:42 -0500

    Play a quiz completed

--- src/app/application/quizzes/[quizzSlug]/page.jsx
+++ src/app/application/quizzes/[quizzSlug]/page.jsx
@@ -2,24 +2,30 @@ import React from "react";
 import QuestionsList from "../../../_components/questions/QuestionsList";
 import { createClient } from "@/app/_lib/supabase/server";
 
+//Page to play a quiz
 const QuizPage = async ({ params }) => {
   const { quizzSlug } = params;
   const supabase = createClient();
 
+  // Get the user
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
+  // Get the quiz
   const { data: quiz, error } = await supabase
     .from("quizzes")
     .select("*")
     .eq("slug", quizzSlug)
     .single();
+  if (error) console.log("[GET QUIZ]", error);
 
-  const { data: questions } = await supabase
+  // Get all the questions for the quiz
+  const { data: questions, error: questionsError } = await supabase
     .from("questions")
     .select("*")
     .eq("quizz_id", quiz.id);
+  if (questionsError) console.log("[GET QUESTIONS]", questionsError);
 
   return (
     <div>
