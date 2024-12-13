4c9faa1e501a671e075b8975958b1dff65bec7d8
ElyzabelleRollin <1720870@cstj.qc.ca>
2024-12-13 17:36:55 -0500

    Search bar fixed

--- src/app/_components/quizzes/QuizzesList.jsx
+++ src/app/_components/quizzes/QuizzesList.jsx
@@ -6,14 +6,24 @@ import styles from "./QuizzesList.module.css";
 //Shows every quiz unless it's banned:
 const QuizzesList = async ({ children, quizzes, amount }) => {
   return (
-    <div className={styles.list}>
-      {children}
-      {quizzes.map((quizz, id) =>
-        quizz.banned ? null : amount && id >= amount ? null : (
-          <Quizcard key={quizz.id} quiz={quizz} />
-        )
-      )}
-    </div>
+    <InfiniteScroll
+      className={styles.list}
+      dataLength={items.length} // number of items in the list
+      next={loadThings} // function to call when more data is needed
+      hasMore={hasMoreQuizzes}
+      loader={<h4>Loading...</h4>} // Loading spinner or message
+      height={400} // Height of the scrollable area
+      
+      endMessage={<p>Yay! You have seen it all</p>} // End of the list message
+    >
+    {items.length > 0 ? (
+  items.map((quiz) => (
+    <Quizcard key={quiz.id} quiz={quiz} />
+  ))
+) : (
+  <p>No quizzes created yet</p>
+)}
+    </InfiniteScroll>
   );
 };
 export default QuizzesList;
