179306149d48e7cd05e1abe4442aacf2ce797a63
SamuelLeduc <2128890@cstj.qc.ca>
2024-12-15 11:17:06 -0500

    added-img-bg-to-quiz-cards

--- src/app/_components/quizzes/Quizcard.jsx
+++ src/app/_components/quizzes/Quizcard.jsx
@@ -3,9 +3,25 @@ import Link from "next/link";
 import styles from "./Quizcard.module.css";
 import Tertiarybutton from "../tertiarybutton/tertiarybutton";
 
+const img1 = "https://utfs.io/f/OJp1c0WpBPn0kKwPvP57sGifEKcJg3aZhTW16SDjr5uRNVQ9";
+const img2 = "https://utfs.io/f/OJp1c0WpBPn0jII6CBdkI7bQlj2KsdM1huw5S9ncGXiaxOoe";
+const img3 = "https://utfs.io/f/OJp1c0WpBPn098Y6bSA2afMwtFmke6nGb3gTSzZlsIPj5hU8";
+
 const Quizcard = async ({ quiz }) => {
+	let rand = Math.floor(Math.random() * 3);
+	let img;
+
+	if (rand === 0) {
+		img = img1;
+	} else if (rand === 1) {
+		img = img2;
+	} else {
+		img = img3;
+	}
+
 	return (
 		<div className={styles.card}>
+			<img src={img} alt="card image" className={styles.img} />
 			<h2 className={styles.title}>{quiz.name}</h2>
 			<p className={styles.description}>{quiz.description}</p>
 			<div className={styles.btn}>
