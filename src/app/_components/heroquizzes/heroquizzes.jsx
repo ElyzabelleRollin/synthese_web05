d345484d8d56f8ba42ad80236f20f9fb74a565ae
amelie <1951472@cstj.qc.ca>
2024-12-15 15:26:46 -0500

    fix-badges-hopefully-last-time

--- src/app/_components/heroquizzes/heroquizzes.jsx
+++ src/app/_components/heroquizzes/heroquizzes.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 import styles from './heroquizzes.module.css'
 import PrimaryButton from '../primarybutton/primarybutton'
 import SecondaryButton from '../secondarybutton/secondarybutton'
+import { createClient } from '@/app/_lib/supabase/server'
 
-const HeroQuizzes = () => {
+const HeroQuizzes = async () => {
+    const supabase = createClient();
+
+    //Get the user
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    //fetch the xp from the profile of the user
+    const { data: profile, error } = await supabase
+        .from('profiles')
+        .select('xp')
+        .eq('id', user.id)
+        .single();
 
     return (
         <section className={styles.heroQuizzes}>
@@ -12,7 +26,8 @@ const HeroQuizzes = () => {
                 <h1 className={styles.title}>Choose Your Quiz Adventure!</h1>
                 <p className={styles.text}>Step into the world of fun and learning! Explore a variety of exciting quizzes designed to challenge your mind and spark your curiosity. Whether you're competing with friends, testing your knowledge, or just having a good time, there’s a quiz waiting for everyone. Ready to dive in? Let the games begin!</p>
                 <div className={styles.buttons}>
-                    <PrimaryButton text="Create your own quiz!" iconright="ArrowRight" theme="light" link={"/application/quizzes/create"}/>
+                    {/* Only users with 1000 xp or more can see the create quiz button */}
+                    {profile.xp >= 1000 ? <PrimaryButton text="Create your own quiz!" iconright="ArrowRight" theme="light" link={"/application/quizzes/create"} /> : null}
                 </div>
             </div>
         </section>
